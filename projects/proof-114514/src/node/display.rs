c09100033313b14d4dd69ac773212e26b8680d39
Aster <aster@vers.site>
2023-12-15 13:46:42 +0800

    Support pretty print records

--- projects/proof-114514/src/node/display.rs
+++ projects/proof-114514/src/node/display.rs
@@ -1,3 +1,4 @@
+use std::fmt::Write;
 use super::*;
 
 pub struct WrapDisplay<'i, T> {
@@ -69,39 +70,85 @@ impl Display for Expression {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Atomic { number } => {
-                Display::fmt(number, f)
+                Display::fmt(number, f)?
             }
             Self::Negative { lhs } => {
-                write!(f, "-{lhs}")
+                write!(f, "-{lhs}")?
             }
             Self::Concat { lhs, rhs } => {
-                f.debug_struct("Concat")
-                    .field("lhs", lhs)
-                    .field("rhs", rhs)
-                    .finish()
+                write!(f, "{lhs}{rhs}")?
             }
             Self::Plus { lhs, rhs } => {
-                write!(f, "{lhs}+{rhs}")
+
+
+                write!(f, "{lhs}+{rhs}")?
             }
             Self::Minus { reverse, lhs, rhs } => {
-                f.debug_struct("Minus")
-                    .field("reverse", reverse)
-                    .field("lhs", lhs)
-                    .field("rhs", rhs)
-                    .finish()
+                if *reverse {
+                    if lhs.lower_than_atom() {
+                        write!(f, "-{lhs}+{rhs}")?
+                    } else {
+                        write!(f, "(-{lhs})+{rhs}")?
+                    }
+                }
+                else {
+                    if rhs.lower_than_mul() {
+                        write!(f, "{lhs}-({rhs})")?
+                    } else {
+                        write!(f, "{lhs}-{rhs}")?
+                    }
+                }
             }
             Self::Times { lhs, rhs } => {
-                f.debug_struct("Times")
-                    .field("lhs", lhs)
-                    .field("rhs", rhs)
-                    .finish()
+                if lhs.lower_than_mul() {
+                    write!(f, "({lhs})")?
+                } else {
+                    write!(f, "{lhs}")?
+                }
+                f.write_char('×')?;
+                if rhs.lower_than_mul() {
+                    write!(f, "({rhs})")?
+                } else {
+                    write!(f, "{rhs}")?
+                }
             }
             Self::Divide { lhs, rhs } => {
-                f.debug_struct("Divide")
-                    .field("lhs", lhs)
-                    .field("rhs", rhs)
-                    .finish()
+                if lhs.lower_than_mul() {
+                    write!(f, "({lhs})")?
+                } else {
+                    write!(f, "{lhs}")?
+                }
+                f.write_char('÷')?;
+                if rhs.lower_than_mul() {
+                    write!(f, "({rhs})")?
+                } else {
+                    write!(f, "{rhs}")?
+                }
             }
+        }
+        Ok(())
+    }
+}
+
+impl Expression {
+    fn lower_than_atom(&self) -> bool {
+        match self {
+            Self::Atomic { .. } => {true}
+            _ => false
+        }
+    }
+    fn lower_sub_rev(&self) -> bool {
+        match self {
+            Self::Minus { reverse, .. } => {*reverse}
+            _ => false
+        }
+    }
+    fn lower_than_mul(&self) -> bool {
+        match self {
+            Self::Plus { .. } => {true}
+            Self::Minus { .. } => {true}
+            Self::Divide { .. } => {true}
+            _ => false
         }
     }
 }
